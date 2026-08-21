317adb87422fae68b6eb705d64699d38bcd27950
Willivms <tucorreo@gmail.com>
2026-08-21 02:30:22 -0300

    Corrijo ruta de la api

--- script.js
+++ script.js
@@ -109,7 +109,7 @@ document.addEventListener('DOMContentLoaded', () => {
         btnReservar.disabled = true;
 
         try {
-            const respuesta = await fetch('https://pagina-de-prueba-locobarber.onrender.com', {
+           const respuesta = await fetch('https://pagina-de-prueba-locobarber.onrender.com/api/reservar', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
